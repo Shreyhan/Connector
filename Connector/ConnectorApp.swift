1fa344e765ea278de6d4df18c17ed4785803072d
Shreyhan <shreyhan@gmail.com>
2026-05-05 15:07:29 -0400

    Swift App

---
+++ Connector/ConnectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectorApp.swift
+//  Connector
+//
+//  Created by Shreyhan Lakhina on 5/5/26.
+//
+
+import SwiftUI
+
+@main
+struct ConnectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
