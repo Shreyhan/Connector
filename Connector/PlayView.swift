104b8402785754fb12c5a609c64a581d52ee81fd
Shreyhan <shreyhan@gmail.com>
2026-09-06 16:07:47 -0400

    feat: created timer

--- Connector/PlayView.swift
+++ Connector/PlayView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Combine
 
 struct PlayView: View {
     let levelNum: Int
@@ -13,22 +14,51 @@ struct PlayView: View {
     @State private var level: Level? = nil
     @State private var orbs: [Orb] = []
     
+    // timer
+    @State private var timeRemaining: Double = 1.0
+    @State private var endDate: Date = .now
+    let timer = Timer.publish(every: 0.01, on: .main, in: .common).autoconnect()
+    
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
         VStack {
-            Text("TITLE OF LEVEL")
+            Text("Level: \(levelNum)")
+                .font(.title.bold())
             if let level {
-                OrbGridView(level: level, orbs: orbs)
+                VStack {
+                    OrbGridView(level: level, orbs: orbs)
+                }
+                .padding()
+                .background(
+                    RoundedRectangle(cornerRadius: 16)
+                )
+                .border(Color.gray.opacity(0.4))
+                
             } else {
                 ProgressView()
             }
-            Text("SOMETHIGN HERE")
-            Divider()
+            
+            if let level {
+                VStack(alignment: .leading) {
+                    ProgressView(value: timeRemaining, total: level.timeLimit)
+                        .scaleEffect(x: 1, y: 4, anchor: .center)
+                    Text("\(Int(timeRemaining))s remaining")
+                        .font(.caption2)
+                }
+                .padding()
+            }
         }
         .onAppear {
             let newLevel = generateLevel(level: levelNum)
             level = newLevel
+            timeRemaining = newLevel.timeLimit
+            endDate = .now.addingTimeInterval(newLevel.timeLimit)
             let orbGenerator = OrbGenerator(level: newLevel)
             orbs = orbGenerator.generateOrbs()
+        }
+        .onReceive(timer) { _ in
+            timeRemaining = max(0, endDate.timeIntervalSince(.now))
         }
     }
 }
